1649f0b978f0a0c0dbdfe492ff210be287c1273d
sparkinzy <sparkinzy@163.com>
2021-09-17 18:03:42 +0800

    fix

--- src/DcatDistpickerServiceProvider.php
+++ src/DcatDistpickerServiceProvider.php
@@ -5,22 +5,24 @@ namespace Sparkinzy\Dcat\Distpicker;
 use Dcat\Admin\Extend\ServiceProvider;
 use Dcat\Admin\Admin;
 use Dcat\Admin\Form;
+use Dcat\Admin\Grid\Filter;
 
 class DcatDistpickerServiceProvider extends ServiceProvider
 {
-	protected $js = [
+    protected $js = [
         'js/distpicker.js',
     ];
-	protected $css = [
-	];
 
-	public function init()
-	{
-		parent::init();
-		Admin::requireAssets('@sparkinzy.dcat-distpicker');
-		Form::extend('distpicker', Distpicker::class);
+    public function init()
+    {
+        parent::init();
+        Admin::requireAssets('@select2');
+        Admin::requireAssets('@sparkinzy.dcat-distpicker');
 
-	}
+        Form::extend('distpicker', Distpicker::class);
+        Filter::extend('distpicker', DistpickerFilter::class);
+
+    }
 
 
 
